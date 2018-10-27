1973b12435bf13d47446d9f09341a8368c826b19
Mathieu Olechowski <m.olechowski@hotmail.fr>
2018-10-27 14:57:56 +0200

    Trajet
    Retourne le trajet MELUN - GDL / GDL - MELUN à l'heure actuelle

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const moment = require('moment');
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 3000;
+const PORT = 5700;
 const URL = "https://api.navitia.io/v1/coverage/fr-idf/"
 const TOKEN = "6f5ff13d-2e7e-4bfb-9d12-e29ecfcc7da5"
 const request = require("request-json");
@@ -10,18 +10,24 @@ const client = request.createClient(URL);
 client.setBasicAuth(TOKEN);
 const arriverGDL = "stop_area:OIF:SA:8768600"
 const departMelun = "stop_area:OIF:SA:8768200"
-const heuredepart = "20181028T133000"
+heureDepartMoment = moment().format("YYYYMMDD" + "T" + "HHmmss");
+const heuredepart = heureDepartMoment
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     client.get('journeys' + '?' + 'to=' + arriverGDL + '&' + 'from=' + departMelun + '&' + 'datetime_represents=departure' + '&' + heuredepart + '&').then(result => {
-        res.send(result.body.journeys);
+        res.send("MELUN - GARE DE LYON " + result.body.journeys);
+    }).catch(error => {
+        console.log(error);
+    });
+
+    client.get('journeys' + '?' + 'to=' + departMelun + '&' + 'from=' + arriverGDL + '&' + 'datetime_represents=departure' + '&' + heuredepart + '&').then(result => {
+        res.send("GARE DE LYON - MELUN " + result.body.journeys);
     }).catch(error => {
         console.log(error);
     });
 });
-
 app.listen(PORT, function () {
-    console.log('App listening on port 3000!');
+    console.log('App listening on port ' + PORT + ' !');
 })
